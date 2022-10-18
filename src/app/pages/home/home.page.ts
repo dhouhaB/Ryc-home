01deec0de4bfe3fad5610a7efaf7543f7cc2640d
dhouhaB <dhouha.bessalah@outlook.com>
2022-10-18 22:14:16 +0100

    first commit

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PickupCallCardComponent } from 'src/app/commponents/pickup-call-card/pickup-call-card.component';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
